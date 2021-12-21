a0478c992b140d9b5ca92808b6a51ea48eaa70a4
ALL Free <krishnaspk000@gmail.com>
2021-12-21 21:01:33 +0700

    Language Translation

--- Language Translation.py
+++ Language Translation.py
@@ -6,10 +6,15 @@ import playsound
 
 recognizer = sr.Recognizer()
 translator = googletrans.Translator()
+print('''8888888888888888888888888888888 Shoss your Language 8888888888888888888888888888888888888888888''')
+print(googletrans.LANGUAGES)
+print('''88888888888888888888888888888888888 Shoss your Language 88888888888888888888888888888888888888888888888888''')
 inputr= input("Enter your input languages example en\n")
 output=input("Enter your output languages example en\n")
 input_lang= inputr
 output_lang= output
+
+
 
 try:
     with sr.Microphone() as source:
