6f62a0c58ed735a80258548aa10a9fecb6c26174
ALL Free <krishnaspk000@gmail.com>
2021-12-21 20:51:24 +0700

    Create Language_Translation_python.py

---
+++ Language_Translation_python.py
@@ -0,0 +1,28 @@
+import googletrans
+from googletrans.client import Translator
+import speech_recognition as sr
+import gtts
+import playsound
+
+recognizer = sr.Recognizer()
+translator = googletrans.Translator()
+inputr= input("Enter your input languages example en\n")
+output=input("Enter your output languages example en\n")
+input_lang= inputr
+output_lang= output
+
+try:
+    with sr.Microphone() as source:
+        print('Speak now')
+        voice = recognizer.listen(source)
+        text = recognizer.recognize_google(voice, language=input_lang)
+        print(text)
+except:
+    pass
+
+translated = translator.translate(text,dest=output_lang)
+print(translated.text)
+converted_audio = gtts.gTTS(translated.text, lang=output_lang)
+converted_audio.save('Output.mp3')
+playsound.playsound('Output.mp3')
+
